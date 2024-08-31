40768c513574790c0e7079fcc5e655cd3c4fd675
Harsh Tripathi <alpharsh1@gmail.com>
2024-08-31 23:33:59 +0530

    Update Footer component styling and content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white pt-10 md:px-8 px-5">
+    <footer className="bg-gray-900 text-white pt-10 pb-3 md:px-8 px-5">
       <div className="mx-auto flex flex-col md:flex-row justify-between items-center">
         <div className="mb-8 md:mb-0 relative md:w-1/2">
           <img src="logo.png" alt="logo" className="h-12 mb-4" />
@@ -27,20 +27,20 @@ const Footer = () => {
           <div className="flex mt-4 justify-start">
             <a
               href="https://www.linkedin.com"
-              className="text-gray-400 text-2xl hover:text-white mr-4 bg-gray-300 bg-opacity-20 rounded-full p-2 px-[0.90rem]"
+              className="text-gray-400 md:text-2xl text-xl hover:text-white mr-4 bg-gray-300 bg-opacity-20 rounded-full md:p-2 p-1 md:px-[0.90rem] px-[0.65rem]"
             >
               <i className="fab fa-linkedin-in"></i>
             </a>
             <a
               href="https://www.instagram.com"
-              className="text-gray-400 text-2xl hover:text-white bg-gray-300 bg-opacity-20 rounded-full p-2 px-[0.90rem]"
+              className="text-gray-400 md:text-2xl text-xl hover:text-white bg-gray-300 bg-opacity-20 rounded-full md:p-2 p-1 md:px-[0.90rem] px-[0.65rem]"
             >
               <i className="fab fa-instagram"></i>
             </a>
           </div>
         </div>
       </div>
-      <div className="md:mt-8 mt-5 border-t mb-1 border-gray-700 pt-4 text-center text-gray-500 text-base">
+      <div className="md:mt-8 mt-5 border-t border-gray-700 pt-4 text-center text-gray-500 text-base">
         Made with <span className="text-red-500">❤</span> by alpharsh.
       </div>
     </footer>
