17a4a4adcabbac9aceaa42a2f0f63bd020e6d8fc
Harsh Tripathi <alpharsh1@gmail.com>
2024-09-09 11:45:38 +0530

    UGI logo added as collaboration

--- src/components/Sponsers.jsx
+++ src/components/Sponsers.jsx
@@ -10,6 +10,11 @@ const sponsors = [
     name: "Institution's Innovation Council",
     logo: 'sp2.png',
     link: '#',
+  },
+  {
+    name: "United Group of Institutions",
+    logo: 'ugilogo.png',
+    link: '#',
   }
 ];
 
@@ -18,7 +23,7 @@ const Sponsors = () => {
     <section className="py-16 bg-gradient-to-b from-[#ffe7d3] via-[#ffd8bb] to-[#fdd7b7]">
       <div className="max-w-7xl mx-auto text-center">
         <h2 className="md:text-5xl text-3xl whitetext font-bold mb-14">COLLABORATIONS</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2  gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-3  gap-8">
           {sponsors.map((sponsor) => (
             <a href={sponsor.link} key={sponsor.name} rel="noopener noreferrer">
               <img src={sponsor.logo} alt={sponsor.name} className="mx-auto h-24 transform transition-transform duration-300 hover:scale-110"/>
