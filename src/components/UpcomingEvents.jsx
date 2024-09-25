eced08ae0b78744dee441904cd13e81797aff14b
Rishabh Vaish <rishabhvaish2002@gmail.com>
2024-09-25 23:11:16 +0530

    webinar and tech talk events added

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -4,6 +4,25 @@ import "aos/dist/aos.css";
 
 const events = [
   {
+    title: "Webinar: Freshman Induction by Coding Ninjas",
+    date: "29th September, 2024",
+    time: "11:00 AM - 12:00 PM",
+    location: "Online",
+    description: "Join us for an online session tailored for incoming freshmen eager to start their coding journey! This interactive webinar will cover essential skills and resources for success in programming.Led by Coding Ninjas instructors, you'll gain valuable insights into the tech landscape and career opportunities. Connect with peers, ask questions, and enhance your coding skills for a bright future in tech!",
+    image: "coding_ninjas_webinar.jpg",
+    // image: "tmp2.png",
+    link: "https://tinyurl.com/4xm3f9z7",
+  },
+  {
+    title: "Tech Talks: A Journey through DEVOPS",
+    date: "06 October, 2024",
+    time: "4:00 PM - 5:00 PM (Tentative)",
+    location: "Online",
+    description: "Join us for an online session on October 6, 2024, featuring Prateek Singh from Fountane Inc. This event will delve into the principles and practices of DevOps, covering key concepts like continuous integration and deployment.Gain insights from industry experts on best practices and tools that enhance productivity in software development. This is a great opportunity to connect with peers and expand your understanding of DevOps in today's tech landscape!",
+    image: "techtalk.jpg",
+    // link: "https://united.ac.in/uhack/",
+  },
+  {
     title: "Uhack 3.0 | Hackathon",
     date: "22-23 October, 2024",
     time: "9:00 AM - 7:00 PM",
@@ -12,30 +31,30 @@ const events = [
     image: "hackathon.jpg",
     link: "https://united.ac.in/uhack/",
   },
-  {
-    title: "Workshop",
-    date: "October 2024",
-    time: "9:00 AM - 4:00 PM",
-    location: "UCER Campus",
-    description: "Join our exclusive AI Workshop for members. Learn the latest in AI, participate in hands-on sessions, and network with industry experts. Enhance your skills and connect with peers.",
-    image: "ai-workshop.jpg",
-    link: "#", // Placeholder link for "Coming Soon"
-  },
-  {
-    title: "Tech Talk: Series",
-    date: "November 2024",
-    time: "9:00 AM - 4:00 PM",
-    location: "UCER Campus",
-    description: "Join our exclusive Talk Series by industry experts on cutting-edge technologies, trends, and professional development. Open to members only, this series offers valuable insights and career advancement opportunities.",
-    image: "techtalk.jpg",
-    link: "#", // Placeholder link for "Coming Soon"
-  },
+  // {
+  //   title: "Workshop",
+  //   date: "October 2024",
+  //   time: "9:00 AM - 4:00 PM",
+  //   location: "UCER Campus",
+  //   description: "Join our exclusive AI Workshop for members. Learn the latest in AI, participate in hands-on sessions, and network with industry experts. Enhance your skills and connect with peers.",
+  //   image: "ai-workshop.jpg",
+  //   link: "#", // Placeholder link for "Coming Soon"
+  // },
+  // {
+  //   title: "Tech Talk: Series",
+  //   date: "November 2024",
+  //   time: "9:00 AM - 4:00 PM",
+  //   location: "UCER Campus",
+  //   description: "Join our exclusive Talk Series by industry experts on cutting-edge technologies, trends, and professional development. Open to members only, this series offers valuable insights and career advancement opportunities.",
+  //   image: "techtalk.jpg",
+  //   link: "#", // Placeholder link for "Coming Soon"
+  // },
   {
     title: "Guest Lectures",
     date: "Coming Soon",
     time: "Coming Soon",
     location: "Coming Soon",
-    description: "Coming Soon",
+    // description: "Coming Soon",
     image: "guest.jpg",
     link: "#", // Placeholder link for "Coming Soon"
   },
